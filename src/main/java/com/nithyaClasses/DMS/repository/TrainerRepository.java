626cf52ebb138eedd1914740ab3a634b94bc8616
KarepeN <s540109@nwmissouri.edu>
2021-05-03 01:02:55 -0500

    Trainer

---
+++ src/main/java/com/nithyaClasses/DMS/repository/TrainerRepository.java
@@ -0,0 +1,9 @@
+package com.nithyaClasses.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.nithyaClasses.DMS.Models.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
